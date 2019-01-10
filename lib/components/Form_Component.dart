e73188d8d61eef54b6f16ce29d92e4a1231d1a14
JimmyFok <huojunmin@me.com>
2019-01-10 21:31:09 +0800

    form

--- lib/components/Form_Component.dart
+++ lib/components/Form_Component.dart
@@ -11,9 +11,66 @@ class Form_Component extends StatelessWidget {
           data: Theme.of(context).copyWith(  // 复制主题，这样处理primaryColor会被覆盖，其他都会保留
             primaryColor: Colors.black,  // 因为ThemeDemo用的不再是primaryColor，所以显示那里是显示accentColor的值
           ),
-          child: ThemeDemo()
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[                
+                TextFieldDemo()
+              ],
+            )
+          )
         )
       ),
+    );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  // 文本控制器
+  final textEditingController = TextEditingController();
+
+  @override
+    void dispose() { // dispose的意思是安排，这里的函数相当于dispose方法继承于父类dispose，再自身扩展
+      textEditingController.dispose();
+      super.dispose();
+    }
+  
+  // 初始数据
+  @override
+    void initState() {
+      super.initState();
+      textEditingController.text = '我是初始值';  // 为什么我这里无效
+      textEditingController.addListener(
+        (){
+          debugPrint('输入：${textEditingController.text}');
+        }
+      );
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: TextField(
+        //  onChanged: (value){
+        //    debugPrint('输入内容: $value');
+        //  },
+         onSubmitted: (value){
+           debugPrint('提交内容: $value');
+         },
+         decoration: InputDecoration(
+           icon: Icon(Icons.people),
+           labelText: '用户名',
+           hintText: '请输入用户名',
+           border: InputBorder.none,
+          //  border: OutlineInputBorder(),
+           filled: true,
+
+         ),
+       ),
     );
   }
 }
